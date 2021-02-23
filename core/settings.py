931d431aebd1e1dd32534105e16c6e406fae7fa6
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2021-02-23 15:33:02 -0500

    nc

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=@#06=wfs%az(ug%7=(x(7390)67=ej*b6f@x#1#gr=rgbq7lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
