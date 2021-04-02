36e83af79cc1f58d1d4f616ba50c867c7c83395f
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2021-04-02 18:26:59 +0200

    edit consumers - change db after ws

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,14 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import *
+from .models import Semaphore
+from channels.db import database_sync_to_async
 
 
 class SemaphoreConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.pk_test = self.scope['url_route']['kwargs']['pk_test']
         self.room_group_name = 'semaphore_%s' % self.pk_test
+        self.status = await database_sync_to_async(self.get_status)()
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -14,6 +16,12 @@ class SemaphoreConsumer(AsyncWebsocketConsumer):
         )
 
         await self.accept()
+
+    def get_status(self):
+        semaphores = Semaphore.objects.all()
+        stat = semaphores.filter(controlUrl=self.pk_test)
+        return stat
+
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -25,6 +33,11 @@ class SemaphoreConsumer(AsyncWebsocketConsumer):
         print("New event is received")
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+
+        if message == 'GREEN':
+            self.status = 'Ready'
+
+        await database_sync_to_async(self.status.save)()
 
         await self.channel_layer.group_send(
             self.room_group_name,
