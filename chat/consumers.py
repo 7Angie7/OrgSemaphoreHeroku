f0c3b851b716a7eee62a0a5dbce5d32095abe48a
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2021-04-04 17:22:14 +0200

    fix consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,7 +8,6 @@ class SemaphoreConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.pk_test = self.scope['url_route']['kwargs']['pk_test']
         self.room_group_name = 'semaphore_%s' % self.pk_test
-        self.status = await database_sync_to_async(self.get_status)()
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -16,12 +15,6 @@ class SemaphoreConsumer(AsyncWebsocketConsumer):
         )
 
         await self.accept()
-
-    def get_status(self):
-        semaphores = Semaphore.objects.all()
-        stat = semaphores.filter(controlUrl=self.pk_test)
-        return stat
-
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -33,11 +26,6 @@ class SemaphoreConsumer(AsyncWebsocketConsumer):
         print("New event is received")
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-        if message == 'GREEN':
-            self.status = 'Ready'
-
-        await database_sync_to_async(self.status.save)()
 
         await self.channel_layer.group_send(
             self.room_group_name,
