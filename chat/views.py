539ecd0c4d627e09a8ad79f2f2f10c1b03a69c21
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2021-04-07 12:26:07 +0200

    nc

--- chat/views.py
+++ chat/views.py
@@ -160,27 +160,15 @@ def joinQueueUrl(request, pk_test):
     # try get the last client in database
     try:
         lastClient = QueueClient.objects.last()
+        response = {
+            'msg': "you are not first client"
+        }
 
     # DB is empty - create first client
     except:
         client, created = QueueClient.objects.get_or_create(device=device, semap=semap, queueNum=1)
         response = {
-            'msg': "Change number of queue"
-        }
-        return JsonResponse(response)
-
-    # check if the client is in DB
-    try:
-        client = QueueClient.objects.get(device=device, semap=semap)
-        response = {
-            'msg': "You are already in the queue"
-        }
-
-    # add client to DB
-    except:
-        client, created = QueueClient.objects.get_or_create(device=device, semap=semap, queueNum=lastClient+1)
-        response = {
-            'msg': "Change number of queue"
+            'msg': "First client"
         }
 
     return JsonResponse(response)
